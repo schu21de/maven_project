4c55ef2bd184437449fdcbd2b3a5cde167e734ec
Silas Schulz <silas.schulz@aesculap.de>
2021-11-25 11:21:03 +0100

    added tests for que and util

--- src/test/java/de/hfu/AppTest.java
+++ src/test/java/de/hfu/AppTest.java
@@ -1,8 +1,8 @@
 package de.hfu;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
@@ -16,5 +16,47 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void istErstesHalbjahrTest() {
+        assertTrue(Util.istErstesHalbjahr(1));
+        assertTrue(Util.istErstesHalbjahr(6));
+        assertFalse(Util.istErstesHalbjahr(7));
+        assertFalse(Util.istErstesHalbjahr(12));
+
+        Util.istErstesHalbjahr(-1);
+        Util.istErstesHalbjahr(13);
+        /*try {
+            Util.istErstesHalbjahr(-1);
+            fail();
+        } catch (Exception e) { }
+        try {
+            Util.istErstesHalbjahr(13);
+            fail();
+        } catch (Exception e) { }*/
+    }
+
+    @Test
+    public void queueTest() {
+        try {
+            Queue queue = new Queue(0);
+            fail();
+        } catch (Exception e) { }
+
+        Queue queue = new Queue(3);
+
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        assertEquals(queue.dequeue(), 1);
+        assertEquals(queue.dequeue(), 2);
+        assertEquals(queue.dequeue(), 4);
+
+        try {
+            queue.dequeue();
+            fail();
+        } catch (Exception e) { }
     }
 }
